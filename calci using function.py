89fa67c3950cfa3dfe58cb8065b5a96b3d3f84f3
Saibalu 588 <kondapallibalu1588.sse@saveetha.com>
2022-09-06 12:35:27 +0530

    Add files via upload
    making an calculator using function

---
+++ calci using function.py
@@ -0,0 +1,14 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/calci.py ==
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 3
+Enter first number: 2
+Enter second number: 3
+2.0 * 3.0 = 6.0
+Let's do next calculation? (yes/no): 
